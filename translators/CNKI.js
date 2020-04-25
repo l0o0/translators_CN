daba5f432cb659a1557af4a04a64778108b03006
l0o0 <linxzh1989@gmail.com>
2020-04-25 21:15:54 +0800

    Add CCJD journal

--- translators/CNKI.js
+++ translators/CNKI.js
@@ -9,30 +9,24 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcs",
-	"lastUpdated": "2020-03-05 06:16:10"
+	"lastUpdated": "2020-04-25 12:55:04"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
-
 	CNKI(China National Knowledge Infrastructure) Translator
 	Copyright © 2013 Aurimas Vinckevicius
-
 	This file is part of Zotero.
-
 	Zotero is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-
 	Zotero is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU Affero General Public License for more details.
-
 	You should have received a copy of the GNU Affero General Public License
 	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
-
 	***** END LICENSE BLOCK *****
 */
 
@@ -109,6 +103,7 @@ function getTypeFromDBName(dbname) {
 		SJES: "journalArticle",
 		SJPD: "journalArticle",
 		SSJD: "journalArticle",
+		CCJD: "journalArticle",
 		CDFD: "thesis",
 		CMFD: "thesis",
 		CLKM: "thesis",
