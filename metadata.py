5b69e1989551f854b788137bf70eb123e45f1dd5
Linxzh <linxzh1989@gmail.com>
2022-10-17 16:28:35 +0800

    update metadata.py
    测试脚本

--- metadata.py
+++ metadata.py
@@ -28,6 +28,6 @@ with open("data/translators.json", 'w', encoding='utf-8') as handle:
 try:
     os.system("git add data/translators.json")
     os.system("git commit -m 'Update metadata %s'" % datetime.now())
-    os.system("git push origin master")
+    os.system("git push")
 except:
     print("Error")
