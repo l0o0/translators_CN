35c498c8ce64f64bb3f39d4d955c9236e9132174
Linxzh <linxzh1989@gmail.com>
2022-10-19 10:29:21 +0800

    fix: 保存转换器信息json文件中文乱码问题修复

--- metadata.py
+++ metadata.py
@@ -11,6 +11,7 @@ def read_metadata(filename):
 
 translators = os.listdir("translators")
 translators = [t for t in translators if t.endswith('js') and t != 'RefWorks Tagged.js']
+translators = sorted(translators)
 
 translator_metadata = {}
 for t in translators:
@@ -22,12 +23,4 @@ for t in translators:
 
 with open("data/translators.json", 'w', encoding='utf-8') as handle:
     print(translator_metadata)
-    json.dump(translator_metadata, handle)
-
-
-try:
-    os.system("git add data/translators.json")
-    os.system("git commit -m 'Update metadata %s'" % datetime.now())
-    os.system("git push origin HEAD:master")
-except:
-    print("Error")
+    json.dump(translator_metadata, handle, ensure_ascii=False)
