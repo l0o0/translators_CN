b60cee441c528737984cb41b22194eeea31b609e
jiaojiaodubai <63148861+jiaojiaodubai@users.noreply.github.com>
2023-11-03 00:48:17 +0800

    匹配两个新域名；修复无翻译字段引起的错误；修复翻译字段被自动转换；替换两个测试案例

--- ChinaXiv.js
+++ ChinaXiv.js
@@ -2,14 +2,14 @@
 	"translatorID": "9282aac1-9c13-4591-9c92-4da3a65ff4e5",
 	"label": "ChinaXiv",
 	"creator": "jiaojiaodubai23",
-	"target": "^http://www\\.chinaxiv\\.org",
+	"target": "^https?://(www\\.)?chinaxiv\\.((org)|(las.ac.cn))",
 	"minVersion": "5.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-10-24 12:42:32"
+	"lastUpdated": "2023-11-02 16:45:20"
 }
 
 /*
@@ -108,21 +108,27 @@ async function scrape(doc, url = doc.location.href) {
 			item.archiveID = url.match(/\/abs\/[\d.]+/)[0].substring(5);
 			// Z.debug(item);
 			item.title = doc.querySelector('div.hd > h1').innerText;
-			item.titleTranslation = doc.querySelector('div.hd > p').innerText;
 			item.creators = ZU.xpath(doc, '//div[@class="flex_item content"]/div[@class="bd"][1]//li[1]/a')
 				.map(element => matchCreator((element.textContent)));
 			item.tags = ZU.xpath(doc, '//span[@class="spankwd"]').map(element => ({ tag: element.textContent }));
 			item.abstractNote = doc.querySelector('div.bd > p:nth-child(1) > b').nextSibling.textContent;
-			item.abstractTranslation = doc.querySelector('div.bd > p:nth-child(2) > b').nextSibling.textContent;
 			item.url = url;
+			item.extra = '';
 			let journalNameLable = Array.prototype.find.call(doc.querySelectorAll(
 				'div.bd > ul > li >b'),
 			node => (node.innerText == '期刊：'));
 			if (journalNameLable) {
-				item.publicationTitle = journalNameLable.nextElementSibling.innerText;
+				item.extra += `\npublicationTitle: ${journalNameLable.nextElementSibling.innerText}`;
 			}
 			else {
-				item.publicationTitle = '中国科学院科技论文预发布平台';
+				item.extra += '\npublicationTitle: 中国科学院科技论文预发布平台';
+			}
+			try {
+				item.extra += `\ntitleTranslation: ${doc.querySelector('div.hd > p').innerText}`;
+				item.extra += `\nabstractTranslation: ${ZU.trim(doc.querySelector('div.bd > p:nth-child(2) > b').nextSibling.textContent)}`;
+			}
+			catch (error) {
+				Z.debug("There's no translation.")
 			}
 			let pdfURL = doc.querySelector('div.side div.bd ul:first-of-type li:nth-child(1) a').href;
 			item.attachments.push({
@@ -145,38 +151,25 @@ async function scrape(doc, url = doc.location.href) {
 var testCases = [
 	{
 		"type": "web",
-		"url": "http://www.chinaxiv.org/abs/202303.00566v1",
+		"url": "http://www.chinaxiv.org/home.htm",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://chinaxiv.org/abs/202310.03455",
 		"detectedItemType": "journalArticle",
 		"items": [
 			{
 				"itemType": "preprint",
-				"title": "B微合金化对HK40合金铸造疏松的影响",
-				"creators": [
-					{
-						"lastName": "丁贤飞",
-						"creatorType": "author",
-						"fieldMode": 1
-					},
-					{
-						"lastName": "刘东方",
-						"creatorType": "author",
-						"fieldMode": 1
-					},
-					{
-						"lastName": "郑运荣",
-						"creatorType": "author",
-						"fieldMode": 1
-					},
-					{
-						"lastName": "冯强",
-						"creatorType": "author",
-						"fieldMode": 1
-					}
-				],
-				"abstractNote": "利用SEM, OM和XRD等手段分析了HK40 合金铸件铸造疏松形成原因, 并研究了添加微量B对HK40 合金的凝固组织及疏松形成的影响. 结果表明: HK40 合金铸件主要存在A和B 2 种铸造疏松缺陷. A类疏松主要由于枝晶的快速生长并架桥联接导致架桥枝晶之间区域的补缩不足引起; B类疏松产生原因是相邻枝晶间区域生长的枝晶状M7C3型碳化物堵塞枝晶间补缩通道. B微合金化能降低HK40 合金铸件较强的柱状晶生长趋势, 细化枝晶, 能抑制HK40 合金A类铸造疏松缺陷的产生. 同时, B微合金化增加了HK40 合金枝晶间共晶相的体积分数, 使枝晶间呈枝晶状M7C3型碳化物转变为层片状的M23C6型碳化物析出, 避免碳化物堵塞相邻枝晶间的补缩通道, 因而也减小了B类铸造疏松缺陷的形成倾向.",
-				"archiveID": "202303.00566",
+				"title": "A Conversation with ChatGPT: Dialogue of Civilizations in the Age of AI",
+				"creators": [],
+				"DOI": "10.12074/202310.03413V1",
+				"abstractNote": "Purpose/significance ChatGPT is a chatbot program developed by OpenAI in the United States. Conversations with ChatGPT can shed light on Dialogue of Civilizations in the age of AI. Method/process Currently, GPT-3.5 offers users 30 free query credits per day. By creating an outline for the conversation, Chen Yu engaged in a dialog with ChatGPT on various issues of Dialogue of Civilizations. Result/conclusion Today, the Standard of Civilization has long been abandoned, and the Clash of Civilizations has been widely criticized. In the era of AI, the AI technology represented by ChatGPT can help promote the Dialogue of Civilizations, help realize real-time communication between people of different cultural backgrounds, enhance the understanding and appreciation of different civilizations, and identify and alleviate prejudices in the dialogue of civilizations. At the same time, the AI technology represented by ChatGPT can also help promote Dialogue within Civilizations and play a positive role in resolving civil conflicts, promoting the integration of immigrants, protecting the voices of vulnerable groups, giving full play to the unique value of women, and building an age-friendly society. However, AI technologies must be developed and used with caution and with due regard to ethical considerations, in particular to prevent AI algorithms from perpetuating prejudices and reinforcing existing inequalities.",
+				"archiveID": "202310.03455",
+				"extra": "publicationTitle: 中国科学院科技论文预发布平台",
 				"libraryCatalog": "ChinaXiv",
-				"url": "http://www.chinaxiv.org/abs/202303.00566v1",
+				"shortTitle": "A Conversation with ChatGPT",
+				"url": "https://chinaxiv.org/abs/202310.03455",
 				"attachments": [
 					{
 						"title": "Full Text PDF",
@@ -189,19 +182,19 @@ var testCases = [
 				],
 				"tags": [
 					{
-						"tag": " B微合金化"
+						"tag": "AI"
 					},
 					{
-						"tag": " 硼化物"
+						"tag": "ChatGPT"
 					},
 					{
-						"tag": " 碳化物"
+						"tag": "Clash of Civilizations"
 					},
 					{
-						"tag": " 铸造疏松"
+						"tag": "Dialogue of Civilizations"
 					},
 					{
-						"tag": "HK40 合金"
+						"tag": "Standard of Civilization"
 					}
 				],
 				"notes": [],
@@ -211,33 +204,18 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "http://www.chinaxiv.org/abs/202308.00082",
+		"url": "https://chinaxiv.las.ac.cn/abs/202311.00029",
 		"detectedItemType": "journalArticle",
 		"items": [
 			{
 				"itemType": "preprint",
-				"title": "Gentle代数的矩阵模型及其整体维数",
-				"creators": [
-					{
-						"lastName": "张梦蝶",
-						"creatorType": "author",
-						"fieldMode": 1
-					},
-					{
-						"lastName": "刘雨喆",
-						"creatorType": "author",
-						"fieldMode": 1
-					},
-					{
-						"lastName": "章超",
-						"creatorType": "author",
-						"fieldMode": 1
-					}
-				],
-				"abstractNote": "该文利用gentle代数的矩阵模型刻画了gentle代数上的单模和投射模, 给出了单模的投射分解的矩阵表示. 由此指出gentle代数的整体维数可以由它的矩阵模型所诱导的一类特殊子矩阵序列进行刻画. 该文进一步指出这一类特殊子矩阵序列对应gentle代数的箭图上的极大非平凡forbidden路, 从而得到gentle代数的整体维数等于它的箭图上的极大非平凡forbidden path的长度.",
-				"archiveID": "202308.00082",
+				"title": "神经递质在恐惧记忆去稳定和再巩固中的作用",
+				"creators": [],
+				"abstractNote": "记忆存储在神经细胞间突触连接的强度变化之中，而神经递质在调节神经元突触可塑性方面具有极为重要的作用。表达特定类型神经递质的神经元可以形成特定的神经递质系统，主要有胆碱能、多巴胺能、去甲肾上腺素能、5-羟色胺能和谷氨酸能系统等。多种类型记忆的去稳定(destabilization)过程的研究揭示，乙酰胆碱在包含新异性信息的记忆提取所引发记忆去稳定过程中发挥了重要作用；而高强度恐惧记忆之所以会抵抗记忆去稳定和再巩固，是因为在这类恐惧记忆编码过程中，去甲肾上腺素-蓝斑系统的激活导致的。其他的重要神经递质包括多巴胺、谷氨酸、-氨基丁酸和血清素等，也都在记忆的不同阶段对记忆可塑性产生影响。神经递质在恐惧记忆去稳定和再巩固中发挥了重要作用，但这种作用通常都不是独立产生的，而是交互作用、相互调节的，包括多巴胺-胆碱能交互、5-羟色胺-谷氨酸交互等。分子层面的神经递质的研究可以给恐惧记忆再巩固干预的研究提供很好的思路启示，未来应基于记忆去稳定的分子机制和神经递质的作用，继续探索恐惧记忆去稳定的关键因素和方法，以更好地改进基于记忆再巩固干预的对于创伤后应激障碍的临床治疗。",
+				"archiveID": "202311.00029",
+				"extra": "publicationTitle: 中国科学院科技论文预发布平台\ntitleTranslation: The role of neurotransmitters in fear memory destabilization and reconsolidation\nabstractTranslation: Memory is stored in the strength changes of synaptic connections between neurons, and neurotransmitters play a crucial role in regulating synaptic plasticity. Neurons expressing specific types of neurotransmitters can form distinct neurotransmitter systems, including the dopaminergic, noradrenergic, serotonergic, and glutamatergic systems. Studies on the destabilization processes of various types of memories have revealed the important role of acetylcholine in memory destabilization triggered by the retrieval of novel associative information. The resistance of high-intensity fear memories to destabilization and reconsolidation is attributed to the activation of the noradrenergic-locus coeruleus system during the encoding process of such fear memories. Other important neurotransmitters, such as dopamine, glutamate, gamma-aminobutyric acid (GABA), and serotonin, also exert influences on memory plasticity at different stages of memory formation. Neurotransmitters play significant roles in fear memory destabilization and reconsolidation, but these effects are typically not independent; rather, they involve interactions and mutual regulation, such as dopamine-cholinergic interactions and serotonin-glutamate interactions. Furthermore, this summary elaborates on the roles of the aforementioned neurotransmitters in memory reconsolidation and their interactions. The study of neurotransmitters at the molecular level can provide valuable insights for the investigation of interventions targeting fear memory reconsolidation. In the future, research should continue to explore the key factors and methods underlying fear memory destabilization based on the molecular mechanisms of memory destabilization and the role of neurotransmitters, to improve the clinical treatment of PTSD based on the reconsolidation intervene.",
 				"libraryCatalog": "ChinaXiv",
-				"url": "http://www.chinaxiv.org/abs/202308.00082",
+				"url": "https://chinaxiv.las.ac.cn/abs/202311.00029",
 				"attachments": [
 					{
 						"title": "Full Text PDF",
@@ -250,34 +228,25 @@ var testCases = [
 				],
 				"tags": [
 					{
-						"tag": "同调维数"
+						"tag": "再巩固"
 					},
 					{
-						"tag": "投射分解"
+						"tag": "去稳定"
 					},
 					{
-						"tag": "投射模"
+						"tag": "条件性恐惧记忆"
 					},
 					{
-						"tag": "矩阵表示"
+						"tag": "神经递质"
 					},
 					{
-						"tag": "箭图表示"
+						"tag": "突触可塑性"
 					}
 				],
-				"notes": [
-					{
-						"note": "<p>��2���޸����ݣ������˶���3.3�еļǺ�˵�����޸��˶���3.3���ݵ���������֤�������ύ��chinaXivʱδ�Ǽǵ�������Ϣ��</p>"
-					}
-				],
+				"notes": [],
 				"seeAlso": []
 			}
 		]
-	},
-	{
-		"type": "web",
-		"url": "http://www.chinaxiv.org/home.htm",
-		"items": "multiple"
 	}
 ]
 /** END TEST CASES **/
