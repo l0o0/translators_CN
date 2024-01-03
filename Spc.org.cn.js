273afd154a0983e50588f6f09e4c4a7145908e99
jiaojiaodubai <63148861+jiaojiaodubai@users.noreply.github.com>
2024-01-03 15:23:44 +0800

    feat (Scp.org.cn.js) 支持多条目；统一内部函数；修复一字线问题；更新测试用例

--- Spc.org.cn.js
+++ Spc.org.cn.js
@@ -2,14 +2,14 @@
 	"translatorID": "3cf79f02-f4af-4392-8b84-26c2bdae2607",
 	"label": "Spc.org.cn",
 	"creator": "018<lyb018@gmail.com>",
-	"target": "https?://www\\.spc\\.org\\.cn/online",
+	"target": "https?://www\\.spc\\.org\\.cn",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-10-18 13:34:59"
+	"lastUpdated": "2024-01-03 07:22:12"
 }
 
 /*
@@ -35,122 +35,147 @@
 	***** END LICENSE BLOCK *****
 */
 
-// eslint-disable-next-line
-function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;}function trim(content){return content.replace(/^[\xA0\s]+/gm, '').replace(/[\xA0\s]+$/gm, '').replace(/\n+/g, '\n').replace(/:\n+/g, ': ').replace(/]\n/g, ']').replace(/】\n/g, '】').replace(/\n\/\n/g, '/')}
-
-function detectWeb(_doc, _url) {
-	return 'report';
+function detectWeb(doc, url) {
+	if (url.includes('/online/')) {
+		return 'standard';
+	}
+	else if (getSearchResults(doc, true)) {
+		return 'multiple';
+	}
+	return false;
 }
 
-function doWeb(doc, url) {
-	scrapeSpc(doc, url);
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll('li > a[href*="/online/"]:nth-child(2), .titleft > a[href*="/online/"]:nth-child(2)');
+	for (let row of rows) {
+		let href = row.href;
+		let title = ZU.trimInternal(row.textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
 }
 
-function scrapeSpc(doc, url) {
-	if (!url || url.length <= 0) {
-		return;
-	}
-
-	var itemType = detectWeb(doc, url);
-	var item = new Zotero.Item(itemType);
-
-	item.language = 'zh-CN';
-	item.url = url;
-	item.libraryCatalog = '中国标准在线服务网';
-	item.extra = "Type: standard";
-
-	var ps = doc.querySelectorAll('ul.detailedinfo-content-collapse > li p');
-
-	for (var p of ps) {
-		var tdTitle = p.textContent.replace('：', '').trim();
-		if (!p.nextElementSibling) {
-			continue;
-		}
-
-		var tdContent = p.nextElementSibling.textContent.trim();
-		if (tdContent.length === 0) {
-			continue;
-		}
-
-		switch (tdTitle) {
-			case '标准号':
-				if (tdContent.startsWith('GB')) {
-					// 国标编号使用一字线
-					tdContent = tdContent.replace('-', '—');
-				}
-				item.reportNumber = tdContent;
-				break;
-			case '标准名称':
-				item.title = tdContent;
-				break;
-			case '出版语种':
-				if (tdContent === 'EN') {
-					item.language = 'en';
-				}
-				break;
-			case '发布日期':
-				if (tdContent.length > 0) {
-					item.date = tdContent;
-				}
-				break;
-			case '归口单位':
-				for (var c of tdContent.split('、')) {
-					c = c.replace('(SAC/TC 4)', '');
-					if (c.startsWith('ISO/')) {
-						c = 'ISO';
-					}
-					item.creators.push({
-						lastName: c,
-						creatorType: 'author',
-						fieldMode: 1
-					});
-				}
-				break;
-			default:
-				break;
+async function doWeb(doc, url) {
+	if (detectWeb(doc, url) == 'multiple') {
+		let items = await Zotero.selectItems(getSearchResults(doc, false));
+		if (!items) return;
+		for (let url of Object.keys(items)) {
+			await scrape(await requestDocument(url));
 		}
 	}
-	item.abstractNote = doc.querySelector('.stand-detail-description').textContent.replace(/标准简介|文前页下载| |\n|\t|/g, '')
-		.replace('读者对象：', '\n读者对象：')
-		.replace('适用范围：暂无', '');
-	item.attachments.push({
-		title: 'Snapshot',
-		document: doc
+	else {
+		await scrape(doc, url);
+	}
+}
+
+async function scrape(doc, url = doc.location.href) {
+	var newItem = new Z.Item('standard');
+	newItem.extra = '';
+	const labels = new Labels(doc, '.detailedinfo-content-collapse > li');
+	Z.debug(labels.innerData.map(arr => [arr[0], ZU.trimInternal(arr[1].innerText)]));
+	newItem.title = labels.getWith('标准名称');
+	newItem.abstractNote = text(doc, '.detailedinfo-text');
+	newItem.number = labels.getWith('标准号').replace('-', '—');
+	newItem.status = labels.getWith('标准状态');
+	newItem.date = labels.getWith('发布日期');
+	newItem.url = url;
+	newItem.numPages = labels.getWith('页数');
+	newItem.language = {
+		中文简体: 'zh-CN'
+	}[labels.getWith('出版语种')];
+	newItem.libraryCatalog = '中国标准在线服务网';
+	newItem.extra += addExtra('original-title', labels.getWith('英文名称'));
+	newItem.extra += addExtra('applyDate', labels.getWith('实施日期'));
+	newItem.extra += addExtra('ICS', labels.getWith('标准ICS号'));
+	newItem.extra += addExtra('CCS', labels.getWith('中标分类号'));
+	newItem.extra += addExtra('draftsman', labels.getWith('起草人'));
+	newItem.extra += addExtra('drafting-committee', labels.getWith('起草单位'));
+	newItem.extra += addExtra('presenter', labels.getWith('提出部门'));
+	newItem.extra += addExtra('substitute-for', labels.getWith('替代以下标准'));
+	newItem.extra += addExtra('substitute-by', labels.getWith('被以下标准替代'));
+	newItem.extra += addExtra('reference', labels.getWith('引用标准'));
+	newItem.extra += addExtra('adopted', labels.getWith('采用标准'));
+	newItem.creators.push({
+		firstName: '',
+		lastName: labels.getWith('归口单位').replace(/\(.*\)$/, ''),
+		creatorType: 'author',
+		fieldMode: 1
 	});
-
-	item.complete();
+	newItem.complete();
 }
 
+class Labels {
+	constructor(doc, selector) {
+		this.innerData = [];
+		Array.from(doc.querySelectorAll(selector))
+			.filter(element => element.firstElementChild)
+			.filter(element => !element.querySelector(selector))
+			.filter(element => !/^\s*$/.test(element.textContent))
+			.forEach((element) => {
+				let elementCopy = element.cloneNode(true);
+				let key = elementCopy.removeChild(elementCopy.firstElementChild).innerText.replace(/\s/g, '');
+				this.innerData.push([key, elementCopy]);
+			});
+	}
 
-/** BEGIN TEST CASES **/
+	getWith(label, element = false) {
+		if (Array.isArray(label)) {
+			let result = label
+				.map(aLabel => this.getWith(aLabel, element));
+			result = element
+				? result.find(element => element.childNodes.length)
+				: result.find(element => element);
+			return result
+				? result
+				: element
+					? document.createElement('div')
+					: '';
+		}
+		let pattern = new RegExp(label);
+		let keyValPair = this.innerData.find(element => pattern.test(element[0]));
+		if (element) return keyValPair ? keyValPair[1] : document.createElement('div');
+		return keyValPair
+			? ZU.trimInternal(keyValPair[1].innerText)
+			: '';
+	}
+}
+
+function addExtra(key, value) {
+	return value
+		? `${key}: ${value}\n`
+		: '';
+}/** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
 		"url": "https://www.spc.org.cn/online/75a5d85b40a9a7c6a780bdb4e3c554e7.html",
 		"items": [
 			{
-				"itemType": "report",
-				"title": "信息与文献　参考文献著录规则",
+				"itemType": "standard",
+				"title": "信息与文献 参考文献著录规则",
 				"creators": [
 					{
+						"firstName": "",
 						"lastName": "全国信息与文献标准化技术委员会",
 						"creatorType": "author",
 						"fieldMode": 1
 					}
 				],
 				"date": "2015-05-15",
-				"abstractNote": "适用范围：本标准规定了各个学科、各种类型信息资源的参考文献的著录项目、著录顺序、著录用符号、著录用文字、各个著录项目的著录方法以及参考文献在正文中的标注法。本标准适用于著者和编辑著录参考文献，而不是供图书馆员、文献目录编制者以及索引编辑者使用的文献著录规则。",
-				"extra": "Type: standard",
+				"abstractNote": "本标准规定了各个学科、各种类型信息资源的参考文献的著录项目、著录顺序、著录用符号、著录用文字、各个著录项目的著录方法以及参考文献在正文中的标注法。本标准适用于著者和编辑著录参考文献，而不是供图书馆员、文献目录编制者以及索引编辑者使用的文献著录规则。",
+				"extra": "original-title: Information and documentation—Rules for bibliographic references and citations to information resources\napplyDate: 2015-12-01\nICS: 01.140.20\nCCS: A14\ndraftsman: 段明莲、白光武、陈浩元、刘曙光、曾燕\ndrafting-committee: 北京大学信息管理系、中国科学技术信息研究所、北京师范大学学报(自然科学版)编辑部、北京大学学报(哲学社会科学版)编辑部、中国科学院文献情报中心\npresenter: 全国信息与文献标准化技术委员会(SAC/TC 4)\nsubstitute-for: GB/T 7714-2005\nsubstitute-by: 无\nreference: GB/T 7408-2005 GB/T 28039-2011 ISO 4\nadopted: ISO 690:2010(E)《信息和文献 参考文献和信息资源引用指南》 NEQ 非等效采用",
 				"language": "zh-CN",
 				"libraryCatalog": "中国标准在线服务网",
-				"reportNumber": "GB/T 7714—2015",
+				"numPages": "28 页",
+				"number": "GB/T 7714—2015",
+				"status": "现行",
 				"url": "https://www.spc.org.cn/online/75a5d85b40a9a7c6a780bdb4e3c554e7.html",
-				"attachments": [
-					{
-						"title": "Snapshot",
-						"mimeType": "text/html"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -159,35 +184,65 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "https://www.spc.org.cn/online/dbbd00948cf962988f33ddc307327a60.html",
+		"url": "https://www.spc.org.cn/online/250f1d53f87de1fd3d3c7a01dcb37abc.html",
 		"items": [
 			{
-				"itemType": "report",
-				"title": "Information and documentation — Guidelines for bibliographic references and citations to information resources",
+				"itemType": "standard",
+				"title": "Information and documentation International Standard Book Number (ISBN)",
 				"creators": [
 					{
-						"lastName": "ISO",
+						"firstName": "",
+						"lastName": "IT-019",
 						"creatorType": "author",
 						"fieldMode": 1
 					}
 				],
-				"date": "2021-06-11",
-				"extra": "Type: standard",
-				"language": "en",
+				"date": "2018-05-08",
+				"abstractNote": "暂无",
+				"extra": "original-title: Information and documentation International Standard Book Number (ISBN)\nsubstitute-for: AS/NZS 1519:2006 DR AS/NZS 1519:2018\nsubstitute-by: 无\nreference: 无\nadopted: 无",
 				"libraryCatalog": "中国标准在线服务网",
-				"reportNumber": "ISO 690:2021 EN",
-				"url": "https://www.spc.org.cn/online/dbbd00948cf962988f33ddc307327a60.html",
-				"attachments": [
-					{
-						"title": "Snapshot",
-						"mimeType": "text/html"
-					}
-				],
+				"number": "AS 1519:2018",
+				"status": "现行",
+				"url": "https://www.spc.org.cn/online/250f1d53f87de1fd3d3c7a01dcb37abc.html",
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
 			}
 		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.spc.org.cn/online/ec5a1433fd041050e17cdd5b94499893.html",
+		"items": [
+			{
+				"itemType": "standard",
+				"title": "수질 — 배양에 의한 미생물 검사의 일반 요구사항 및 지침",
+				"creators": [],
+				"date": "2023-11-30",
+				"abstractNote": "暂无",
+				"extra": "original-title: Water quality — General requirements and guidance for microbiological examinations by culture\nICS: 07.100.20\nsubstitute-for: 无\nsubstitute-by: 无\nreference: 无\nadopted: 无",
+				"libraryCatalog": "中国标准在线服务网",
+				"numPages": "63 页",
+				"number": "KS I ISO 8199",
+				"status": "现行",
+				"url": "https://www.spc.org.cn/online/ec5a1433fd041050e17cdd5b94499893.html",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.spc.org.cn/",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.spc.org.cn/basicsearch",
+		"items": "multiple"
 	}
 ]
 /** END TEST CASES **/
