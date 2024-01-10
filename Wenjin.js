a68397fdabd330de2d46bd035eadb8316a0ac6ef
jiaojiaodubai <63148861+jiaojiaodubai@users.noreply.github.com>
2024-01-10 14:03:51 +0800

    修复期刊文章抓取不到作者；修复学位论文日期意外抓取文档元素；优化图书作者显示；更新测试用例

--- Wenjin.js
+++ Wenjin.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2024-01-05 13:53:11"
+	"lastUpdated": "2024-01-10 06:01:34"
 }
 
 /*
@@ -131,7 +131,7 @@ async function scrape(doc, url = doc.location.href) {
 				// http://find.nlc.cn/search/showDocDetails?docId=-9108259640625243569&dataSource=ucs01&query=%E6%A2%A6%E6%BA%AA%E7%AC%94%E8%B0%88
 				// http://find.nlc.cn/search/showDocDetails?docId=-4203196484494800823&dataSource=ucs01&query=%E6%B0%B4%E5%90%88%E7%89%A9
 				// http://find.nlc.cn/search/showDocDetails?docId=-4300747930403378569&dataSource=ucs01&query=%E6%9C%97%E6%96%87
-				creator = creator.replace(/等?\[?[主编著撰翻译]*\]?$/, '');
+				creator = creator.replace(/等?\[?[主编著撰翻译演奏]*\]?$/, '');
 				let country = tryMatch(creator, /^[([（](.+?)[)\]）]/, 1);
 				creator = creator.replace(/^[([（](.+?)[)\]） ]/, '');
 				let original = tryMatch(creator, /\((.+?)\)$/, 1);
@@ -147,9 +147,12 @@ async function scrape(doc, url = doc.location.href) {
 				return creator;
 			});
 			Z.debug(creators);
-			newItem.extra += addExtra('creatorsExt', JSON.stringify(creators));
+			if (creators.some(creator => creator.country || creator.original)) {
+				newItem.extra += addExtra('creatorsExt', JSON.stringify(creators));
+			}
 			creators.forEach((creator) => {
 				delete creator.country;
+				newItem.extra += addExtra('original-author', creator.original);
 				delete creator.original;
 				newItem.creators.push(creator);
 			});
@@ -172,31 +175,43 @@ async function scrape(doc, url = doc.location.href) {
 			// newItem.volume = 卷次;
 			newItem.issue = tryMatch(labels.getWith('期'), /0*([1-9]\d*)/, 1);
 			newItem.pages = labels.getWith('页');
-			newItem.date = labels.getWith('年');
+			newItem.date = labels.getWith(['出版发行时间', '年']);
 			newItem.ISSN = tryMatch(labels.getWith('标识号'), /:(.+?)(\s|$)/, 1);
 			newItem.extra += addExtra('original-title', tryMatch(labels.getWith('文章题名'), /英文篇名 : (.+)(?:\s{2}|$)/, 1));
 			newItem.extra += addExtra('fund', labels.getWith('基金'));
+			new Labels(doc, '#detail-info > .book_item').getWith('作者', true).innerText.trim()
+				.split(/\s{2}|，/).forEach((creator) => {
+					Z.debug(creator);
+					creator = ZU.cleanAuthor(creator, 'author');
+					if (/[\u4e00-\u9fa5]/.test(creator.lastName)) {
+						creator.lastName = creator.firstName + creator.lastName;
+						creator.firstName = '';
+						creator.fieldMode = 1;
+					}
+					newItem.creators.push(creator);
+				});
 			break;
 		case 'thesis':
 			newItem.thesisType = labels.getWith('来源数据库').includes('博士')
 				? '博士学位论文'
 				: '硕士学位论文';
 			newItem.university = labels.getWith('论文授予机构');
-			newItem.date = labels.getWith('出版发行时间', '论文授予时间');
+			newItem.date = labels.getWith(['出版发行时间', '论文授予时间']);
 			newItem.numPages = tryMatch(labels.getWith('载体形态'), /(\d+)页$/, 1);
-			labels.getWith('所有责任者', true).innerText.trim().split(/\s{2}|，/).forEach((creator) => {
-				let creatorType = /指导$/.test(creator)
-					? 'contributor'
-					: 'author';
-				creator = creator.replace(/等?[著指导]*$/, '');
-				creator = ZU.cleanAuthor(creator, creatorType);
-				if (/[\u4e00-\u9fa5]/.test(creator.lastName)) {
-					creator.lastName = creator.firstName + creator.lastName;
-					creator.firstName = '';
-					creator.fieldMode = 1;
-				}
-				newItem.creators.push(creator);
-			});
+			labels.getWith('所有责任者', true).innerText.trim()
+				.split(/\s{2}|，/).forEach((creator) => {
+					let creatorType = /指导$/.test(creator)
+						? 'contributor'
+						: 'author';
+					creator = creator.replace(/等?[著指导]*$/, '');
+					creator = ZU.cleanAuthor(creator, creatorType);
+					if (/[\u4e00-\u9fa5]/.test(creator.lastName)) {
+						creator.lastName = creator.firstName + creator.lastName;
+						creator.firstName = '';
+						creator.fieldMode = 1;
+					}
+					newItem.creators.push(creator);
+				});
 			newItem.extra += addExtra('subject', labels.getWith('论文专业 '));
 			break;
 		case 'newspaperArticle':
@@ -206,17 +221,18 @@ async function scrape(doc, url = doc.location.href) {
 			newItem.publicationTitle = labels.getWith(['报纸中文名', '期刊名称']);
 			newItem.date = labels.getWith(['日期', '出版发行扇门']);
 			newItem.pages = tryMatch(labels.getWith('来源'), /(\d+)版/, 1) || labels.getWith('版号');
-			labels.getWith(['所有责任者', '作者']).split(/\s/).filter(creator => !creator.includes('记者'))
-.forEach((creator) => {
-	Z.debug(creator);
-	creator = ZU.cleanAuthor(creator, 'author');
-	if (/[\u4e00-\u9fa5]/.test(creator.lastName)) {
-		creator.lastName = creator.firstName + creator.lastName;
-		creator.firstName = '';
-		creator.fieldMode = 1;
-	}
-	newItem.creators.push(creator);
-});
+			labels.getWith(['所有责任者', '作者']).split(/\s/)
+				.filter(creator => !creator.includes('记者'))
+				.forEach((creator) => {
+					Z.debug(creator);
+					creator = ZU.cleanAuthor(creator, 'author');
+					if (/[\u4e00-\u9fa5]/.test(creator.lastName)) {
+						creator.lastName = creator.firstName + creator.lastName;
+						creator.firstName = '';
+						creator.fieldMode = 1;
+					}
+					newItem.creators.push(creator);
+				});
 			break;
 		case 'report':
 			break;
@@ -313,28 +329,21 @@ var testCases = [
 					}
 				],
 				"date": "2013-02-28",
+				"ISSN": "1674-568X",
 				"abstractNote": "WGCNA(weighted geneco-expression network analysis)算法是一种构建基因共表达网络的典型系统生物学算法,该算法基于高通量的基因信使RNA(mRNA)表达芯片数据,被广泛应用于国际生物医学领域。本文旨在介绍WGCNA的基本数理原理,并依托R软件包WGNCA以实例的方式介绍其应用。WGCNA算法首先假定基因网络服从无尺度分布,并定义基因共表达相关矩阵、基因网络形成的邻接函数,然后计算不同节点的相异系数,并据此构建分层聚类树(hierarchical clusteringtree),该聚类树的不同分支代表不同的基因模块(module),模块内基因共表达程度高,而分数不同模块的基因共表达程度低。最后,探索模块与特定表型或疾病的关联关系,最终达到鉴定疾病治疗的靶点基因、基因网络的目的。",
-				"issue": "01",
+				"extra": "original-title: Gene Co-expression Network Analysis Based on WGCNA Algorithm-Theory and Implementation in R Software\nfund: 青海省135高层次人才培养基金资助",
+				"issue": "1",
 				"libraryCatalog": "Wenjin",
 				"pages": "143-149",
-				"publicationTitle": "基因组学与应用生物学 Genomics and Applied Biology",
+				"publicationTitle": "基因组学与应用生物学",
 				"url": "http://find.nlc.cn/search/showDocDetails?docId=-8373230212045865087&dataSource=cjfd&query=wgcna",
 				"attachments": [],
 				"tags": [
 					{
-						"tag": " Gene co"
-					},
-					{
-						"tag": " R software"
-					},
-					{
-						"tag": "R软件WGCNA"
+						"tag": "R软件"
 					},
 					{
 						"tag": "WGCNA算法"
-					},
-					{
-						"tag": "expression network"
 					},
 					{
 						"tag": "基因共表达网络"
@@ -369,7 +378,7 @@ var testCases = [
 				"date": "2012",
 				"ISBN": "9787506361347",
 				"abstractNote": "本书分为十二篇，内容包括：探寻红色中国、去红都的道路、在保安、一个共产党员的由来等。",
-				"extra": "creatorsExt: [{\"firstName\":\"\",\"lastName\":\"埃德加·斯诺\",\"creatorType\":\"author\",\"fieldMode\":1,\"country\":\"美\",\"original\":\"Edgar Snow\"},{\"firstName\":\"\",\"lastName\":\"董乐山\",\"creatorType\":\"translator\",\"fieldMode\":1,\"country\":\"\",\"original\":\"\"}]",
+				"extra": "creatorsExt: [{\"firstName\":\"\",\"lastName\":\"埃德加·斯诺\",\"creatorType\":\"author\",\"fieldMode\":1,\"country\":\"美\",\"original\":\"Edgar Snow\"},{\"firstName\":\"\",\"lastName\":\"董乐山\",\"creatorType\":\"translator\",\"fieldMode\":1,\"country\":\"\",\"original\":\"\"}]\noriginal-author: Edgar Snow",
 				"language": "zh-CN",
 				"libraryCatalog": "Wenjin",
 				"numPages": "338",
@@ -385,7 +394,10 @@ var testCases = [
 						"tag": "史料"
 					},
 					{
-						"tag": "史料 陕北革命根据地"
+						"tag": "史料"
+					},
+					{
+						"tag": "陕北革命根据地"
 					}
 				],
 				"notes": [],
@@ -414,7 +426,7 @@ var testCases = [
 						"fieldMode": 1
 					}
 				],
-				"date": "[object HTMLDivElement]",
+				"date": "2004",
 				"language": "zh-CN",
 				"libraryCatalog": "Wenjin",
 				"numPages": "289",
@@ -465,10 +477,11 @@ var testCases = [
 						"fieldMode": 1
 					}
 				],
-				"date": "[object HTMLDivElement]",
+				"date": "2004",
 				"language": "zh-CN",
 				"libraryCatalog": "Wenjin",
 				"numPages": "289",
+				"thesisType": "博士学位论文",
 				"university": "中国社会科学院",
 				"url": "http://find.nlc.cn/search/showDocDetails?docId=7225006674714026291&dataSource=ucs01,bslw&query=%E4%BF%A1%E7%94%A8",
 				"attachments": [],
@@ -531,14 +544,14 @@ var testCases = [
 				"creators": [
 					{
 						"firstName": "",
-						"lastName": "理查德·克莱德曼演奏",
+						"lastName": "理查德·克莱德曼",
 						"creatorType": "author",
 						"fieldMode": 1
 					}
 				],
 				"date": "2008",
 				"ISBN": "9787884737703",
-				"extra": "creatorsExt: [{\"firstName\":\"\",\"lastName\":\"理查德·克莱德曼演奏\",\"creatorType\":\"author\",\"fieldMode\":1,\"country\":\"法\",\"original\":\"\"}]\nmedium: CD",
+				"extra": "creatorsExt: [{\"firstName\":\"\",\"lastName\":\"理查德·克莱德曼\",\"creatorType\":\"author\",\"fieldMode\":1,\"country\":\"法\",\"original\":\"\"}]\nmedium: CD",
 				"libraryCatalog": "Wenjin",
 				"place": "武汉",
 				"publisher": "扬子江音像出版社",
